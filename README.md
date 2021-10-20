964323b3d48e8bec0bc8f0c66c7e803f5c0afad9
adreider <adreidereconomic@gmail.com>
2021-10-20 10:23:06 -0300

    create messageController and  middleware ensureAuthenticated

---
+++ README.md
@@ -0,0 +1,8 @@
+# NLW heat
+
+Projeto desenvolvido no evento NLW HEAT da Rocketseat 💜
+
+## tenologias 🚀
+ - prisma
+ - sqlite
+ - github-api
--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -4,14 +4,21 @@ import { AuthenticateUserService } from '../services/AuthenticateUserService';
 
 class AuthenticateUserController {
   async handle(request: Request, response: Response) {
-    
+
     const { code } = request.body;
 
     const service = new AuthenticateUserService();
-    const result =  await service.execute(code);
+    try {
+      
+      const result = await service.execute(code);
+      
+      return response.json(result);
 
-    return response.json(result);
-    
+    } catch (err) {
+      return response.json({error: err.message});
+    }
+
+
   }
 }
 

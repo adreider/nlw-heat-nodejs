46e358bcba3f2d32f70e7cbdc03dd44219b83dfd
adreider <adreidereconomic@gmail.com>
2021-10-22 16:58:58 -0300

    in deploy

---
+++ dist/server.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+app_1.serverHttp.listen(process.env.PORT || 4000, () => console.log(`🚀 Server is running on port (4000)`));
